61c19759cc2f87f313633746eaa81009045cf995
Jairo <jromero.alumno@iesrafaelalberti.es>
2022-10-25 14:22:21 +0200

    creador

--- codigophp/recibir-json.php
+++ codigophp/recibir-json.php
@@ -1,4 +1,5 @@
 <?php
+require("pruebalogin.php");
 //url for meme list
 $url = 'https://api.imgflip.com/get_memes';
 
@@ -22,6 +23,6 @@ if($data["success"]) {
     //iterates over memes array
     foreach($data["data"]["memes"] as $meme) {
         //show meme image
-        echo "<a href='creador.php' target='crear'><img width='100px' href='creador.php' src='" . $meme["url"] . "'></a>";
+        echo "<a href='creador.php?id=".$meme["id"]."&boxes=".$meme["box_count"]."&url=".$meme["url"]."'><img width='100px' href='creador.php' src='" . $meme["url"] . "'></a>";
     }
 }
